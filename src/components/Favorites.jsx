b099ee8592778ca27488508fd0592c97d3c67419
Muhammet Aksu <muhammetaksu.95@gmail.com>
2022-07-15 22:10:20 +0300

    favorites page fixed

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -47,159 +47,161 @@ function Favorites() {
     };
 
     return (
-        <div style={{ minHeight: "70vh" }} className="container p-4">
-            <div
-                id="goTo"
-                className="position-fixed"
-                style={{
-                    bottom: "30px",
-                    right: "20px",
-                    zIndex: "1",
-                }}
-            >
-                <button
-                    id="goBtn"
-                    onClick={() => goToTop()}
-                    className="d-inline rounded-top border-bottom border-0  "
+        <>
+            <div style={{ minHeight: "65vh" }} className="container p-4">
+                <div
+                    id="goTo"
+                    className="position-fixed"
+                    style={{
+                        bottom: "30px",
+                        right: "20px",
+                        zIndex: "1",
+                    }}
                 >
-                    <ArrowDropUpIcon className="fs-2" />
-                </button>
-                <br />
-                <button
-                    id="goBtn"
-                    onClick={() => goToBottom()}
-                    className="d-inline rounded-bottom border-top border-0  "
-                >
-                    <ArrowDropDownIcon className="fs-2" />
-                </button>
-            </div>
-
-            <div>
-                <h2 className="my-4 fav-heads rounded border-bottom shadow">
-                    Favorite Books
-                </h2>
-            </div>
-            <div className="col-2 d-flex justify-content-center ms-auto">
-                <Typography className="fw-bold text-center bg-light rounded p-2">
-                    Page {pageNumber}
-                </Typography>
-            </div>
-
-            <div className="row ">
-                {loaderIsActive === true ? (
-                    <div
-                        style={{ height: "55vh" }}
-                        className="d-flex justify-content-center my-5"
+                    <button
+                        id="goBtn"
+                        onClick={() => goToTop()}
+                        className="d-inline rounded-top border-bottom border-0  "
                     >
-                        <div className="my-auto">{tailSpinLoading}</div>
-                    </div>
-                ) : displayBooks.length > 0 ? (
-                    displayBooks.map((book) => (
+                        <ArrowDropUpIcon className="fs-2" />
+                    </button>
+                    <br />
+                    <button
+                        id="goBtn"
+                        onClick={() => goToBottom()}
+                        className="d-inline rounded-bottom border-top border-0  "
+                    >
+                        <ArrowDropDownIcon className="fs-2" />
+                    </button>
+                </div>
+
+                <div>
+                    <h2 className="my-4 fav-heads rounded border-bottom shadow">
+                        Favorite Books
+                    </h2>
+                </div>
+                <div className="col-2 d-flex justify-content-center ms-auto">
+                    <Typography className="fw-bold text-center bg-light rounded p-2">
+                        Page {pageNumber}
+                    </Typography>
+                </div>
+
+                <div className="row ">
+                    {loaderIsActive === true ? (
                         <div
-                            key={book.id}
-                            className="col-12  d-flex justify-content-center mt-3 mb-3"
+                            style={{ height: "55vh" }}
+                            className="d-flex justify-content-center my-5"
                         >
-                            <div className=" row border rounded shadow">
-                                <div
-                                    id="favoritesImg"
-                                    className="col-lg-3 col-md-4 d-flex ps-0 justify-center-start"
-                                >
-                                    {window.innerWidth < 768 ? (
-                                        <div>
-                                            <img
-                                                style={{
-                                                    height: "350px",
-                                                }}
-                                                src={book.image_url}
-                                                className="img-fluid"
-                                                alt="..."
-                                            />
-                                        </div>
-                                    ) : (
-                                        <div>
-                                            <img
-                                                style={{
-                                                    height: "350px",
-                                                }}
-                                                src={book.image_url}
-                                                className="img-fluid rounded-start"
-                                                alt="..."
-                                            />
-                                        </div>
-                                    )}
-                                </div>
-                                <div className="col-lg-3 col-md-3 py-3">
-                                    <div className="row">
-                                        <div className="p-0">
-                                            <h5 className="card-title px-2">
-                                                <span className=" fs-5 rounded fav-heads border-bottom mb-2">
-                                                    Name:
-                                                </span>
-                                                <br />
-                                                {book.title}
-                                            </h5>
-                                            <hr />
-                                        </div>
+                            <div className="my-auto">{tailSpinLoading}</div>
+                        </div>
+                    ) : displayBooks.length > 0 ? (
+                        displayBooks.map((book) => (
+                            <div
+                                key={book.id}
+                                className="col-12  d-flex justify-content-center mt-3 mb-3"
+                            >
+                                <div className=" row border rounded shadow">
+                                    <div
+                                        id="favoritesImg"
+                                        className="col-lg-3 col-md-4 d-flex ps-0 justify-center-start"
+                                    >
+                                        {window.innerWidth < 768 ? (
+                                            <div>
+                                                <img
+                                                    style={{
+                                                        height: "350px",
+                                                    }}
+                                                    src={book.image_url}
+                                                    className="img-fluid"
+                                                    alt="..."
+                                                />
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                <img
+                                                    style={{
+                                                        height: "350px",
+                                                    }}
+                                                    src={book.image_url}
+                                                    className="img-fluid rounded-start"
+                                                    alt="..."
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                    <div className="col-lg-3 col-md-3 py-3">
+                                        <div className="row">
+                                            <div className="p-0">
+                                                <h5 className="card-title px-2">
+                                                    <span className=" fs-5 rounded fav-heads border-bottom mb-2">
+                                                        Name:
+                                                    </span>
+                                                    <br />
+                                                    {book.title}
+                                                </h5>
+                                                <hr />
+                                            </div>
 
-                                        <div className="p-0">
-                                            <h5 className="card-title px-2">
-                                                <span className=" fs-5 rounded fav-heads border-bottom mb-2">
-                                                    Author:
-                                                </span>
-                                                <br />
-                                                {book.authors}
-                                            </h5>
-                                            <hr />
-                                        </div>
+                                            <div className="p-0">
+                                                <h5 className="card-title px-2">
+                                                    <span className=" fs-5 rounded fav-heads border-bottom mb-2">
+                                                        Author:
+                                                    </span>
+                                                    <br />
+                                                    {book.authors}
+                                                </h5>
+                                                <hr />
+                                            </div>
 
-                                        <div className="p-0">
-                                            <h5 className="card-title px-2">
-                                                <span className=" fs-5 rounded fav-heads border-bottom mb-2">
-                                                    Genres:
-                                                </span>
-                                                <br />
-                                                {book.genres}
-                                            </h5>
+                                            <div className="p-0">
+                                                <h5 className="card-title px-2">
+                                                    <span className=" fs-5 rounded fav-heads border-bottom mb-2">
+                                                        Genres:
+                                                    </span>
+                                                    <br />
+                                                    {book.genres}
+                                                </h5>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                <div className="col-lg-6 col-md-5 py-3">
-                                    <div className="card-body">
-                                        <h5 className="fs-5 rounded fav-heads border-bottom">
-                                            Description
-                                        </h5>
-                                        <div className="mb-2">
-                                            <p
-                                                style={{
-                                                    height: "220px",
-                                                    overflowY: "auto",
-                                                }}
-                                                className="card-text px-3"
+                                    <div className="col-lg-6 col-md-5 py-3">
+                                        <div className="card-body">
+                                            <h5 className="fs-5 rounded fav-heads border-bottom">
+                                                Description
+                                            </h5>
+                                            <div className="mb-2">
+                                                <p
+                                                    style={{
+                                                        height: "220px",
+                                                        overflowY: "auto",
+                                                    }}
+                                                    className="card-text px-3"
+                                                >
+                                                    {book.description}
+                                                </p>
+                                            </div>
+                                            <button
+                                                onClick={() =>
+                                                    removeToFavorites(book)
+                                                }
+                                                className="btn btn-warning mt-3"
                                             >
-                                                {book.description}
-                                            </p>
+                                                Remove from Favorites
+                                            </button>
                                         </div>
-                                        <button
-                                            onClick={() =>
-                                                removeToFavorites(book)
-                                            }
-                                            className="btn btn-warning mt-3"
-                                        >
-                                            Remove from Favorites
-                                        </button>
                                     </div>
                                 </div>
                             </div>
+                        ))
+                    ) : (
+                        <div className="container d-flex justify-content-center">
+                            <h5 className=" m-5 p-3 fav-heads rounded border-bottom shadow">
+                                Favorite list is empty.
+                            </h5>
                         </div>
-                    ))
-                ) : (
-                    <div className="container d-flex justify-content-center">
-                        <h5 className=" m-5 p-3 fav-heads rounded border-bottom shadow">
-                            Favorite list is empty.
-                        </h5>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
             <Stack spacing={2}>
                 <Pagination
@@ -221,7 +223,7 @@ function Favorites() {
                     )}
                 />
             </Stack>
-        </div>
+        </>
     );
 }
 export default Favorites;
